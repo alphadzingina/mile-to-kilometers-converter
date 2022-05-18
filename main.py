8820d5b93f355526f3d68750e873bc3bacd069f3
alphadzingina <alphaadx6@gmail.com>
2022-05-18 02:11:52 +0100

    add conversion component

--- main.py
+++ main.py
@@ -10,11 +10,20 @@ miles_entry = Entry(width=30)
 miles_entry.insert(END, string="0")
 #Gets text in entry
 print(miles_entry.get())
-miles_entry.pack()
-
+miles_entry.grid(column=1, row=0)
 
 miles_label = Label(text="Miles")
-miles_label.pack()
+miles_label.grid(column=2, row=0)
+
+
+#conversion component
+equal_label = Label(text="is equal to:")
+equal_label.grid(column=0, row=1)
+kilometers_label = Label(text="0")
+kilometers_label.grid(column=1, row=1)
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
 
 
 
